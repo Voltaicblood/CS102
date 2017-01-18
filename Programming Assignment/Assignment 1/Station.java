f4bb324f4fb3b0a8e673625a7f3856e6480b267c
Voltaicblood <ajayye@hotmail.com>
2017-01-18 15:08:29 -0500

    Init

---
+++ Programming Assignment/Assignment 1/Station.java
@@ -0,0 +1,8 @@
+/**************************************************************************/
+/* Jacob Austin                                                           */
+/* Login ID: aust8558                                                     */ 
+/* CS-102, Winter 2017                                                    */
+/* Programming Assignment 1                                               */
+/* Station class: contains members corresponding to a given radio station */
+/**************************************************************************/
+
